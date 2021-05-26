ca99ad2c93ec7a9a392ae1e3a4363a786846a16c
bigOrange123456789 <2814255951@qq.com>
2021-05-26 16:48:25 +0800

    save

--- blender/ModelProcessingTool.py
+++ blender/ModelProcessingTool.py
@@ -11,17 +11,21 @@ class ModelProcessingTool:
     def start(self):
         bpy.ops.object.mode_set(mode='OBJECT')
         self.merge()
-        for obj in bpy.data.objects:
-            if obj.type=="MESH":
-                self.simplification(0.1,obj)
-        self.separate()
+        self.simplification_all()
+        self.separate('MATERIAL')
         self.reName()
-
+        self.separate('LOOSE')
+        self.download("E:\\myFile\\test\\test")
 
     def merge(self):
         bpy.ops.object.select_all(action='DESELECT')#取消选择
         bpy.ops.object.select_by_type(type='MESH')#选中所有mesh对象
         bpy.ops.object.join()#合并
+
+    def simplification_all(self):
+        for obj in bpy.data.objects:
+            if obj.type=="MESH":
+                self.simplification(0.1,obj)
 
     def simplification(self,r,obj):
         obj.modifiers.new("dec", type = "DECIMATE")#decimate 毁灭#精简
@@ -30,9 +34,9 @@ class ModelProcessingTool:
 
         bpy.ops.object.modifier_apply(modifier='dec')
 
-    def separate(self):
+    def separate(self,type0):
         bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.separate(type="LOOSE")#SELECTED
+        bpy.ops.mesh.separate(type=type0)#SELECTED  "LOOSE"
         bpy.ops.object.mode_set(mode='OBJECT')
 
     def reName(self):
@@ -41,5 +45,8 @@ class ModelProcessingTool:
             if i.type == 'MESH':
                 i.name ="mesh"+str(k0);# "改名网格物体"+i.name
                 k0=k0+1;
+    def download(self,url):
+        bpy.ops.export_scene.gltf(filepath=url, export_format="GLB", export_tangents=False,
+                             export_image_format="JPEG", export_cameras=False, export_lights=False)
 
 ModelProcessingTool()
